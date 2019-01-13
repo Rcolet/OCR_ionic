c6e4e6823682a289431298c24c44b5aff2f5ed06
Marion Mounaix <mamounaix@airfrance.fr>
2019-01-13 17:13:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,49 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Camera } from '@ionic-native/camera';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { EmailFormPage } from '../pages/email-form/email-form';
+import { EmailProvider } from '../providers/email/email';
+import { EmailComposer } from '@ionic-native/email-composer';
+import { ImageProvider } from '../providers/image/image';
 
+class CameraMock extends Camera {
+  getPicture(options) {
+    return new Promise((resolve, reject) => {
+      resolve("BASE_64_ENCODED_DATA_GOES_HERE");
+    })
+  }
+}
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EmailFormPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgProgressModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EmailFormPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    EmailComposer,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: Camera, useClass: CameraMock },
+    EmailProvider,
+    ImageProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,91 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera';
+import * as Tesseract from 'tesseract.js'
+import { NgProgress } from '@ngx-progressbar/core';
+import { Platform } from 'ionic-angular';
+import { EmailFormPage } from '../email-form/email-form';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  selectedImage: string;
+  imageText: string;
+  emailFormPage = EmailFormPage;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, 
+              private camera: Camera, 
+              private actionSheetCtrl: ActionSheetController, 
+              public progress: NgProgress,
+              public platform:Platform) {
+  }
+  selectSource() {   
+    if (this.platform.is('core')) {
+      let actionSheet = this.actionSheetCtrl.create({
+        buttons: [
+          {
+            text: 'Use Library',
+            handler: () => {
+              this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+            }
+          }, {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      });
+      actionSheet.present();
+    } else {
+      let actionSheet = this.actionSheetCtrl.create({
+        buttons: [
+          {
+            text: 'Use Library',
+            handler: () => {
+              this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+            }
+          }, {
+            text: 'Capture Image',
+            handler: () => {
+              this.getPicture(this.camera.PictureSourceType.CAMERA);
+            }
+          }, {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      });
+      actionSheet.present();
+    }
   }
 
+  getPicture(sourceType: PictureSourceType) {
+      this.camera.getPicture({
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType: sourceType,
+        allowEdit: true,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+      }).then((imageData) => {
+        this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+      });
+  }
+
+  recognizeImage() {
+    Tesseract.recognize(this.selectedImage)
+    .progress(message => {
+      if (message.status === 'recognizing text')
+      this.progress.set(message.progress);
+    })
+    .catch(err => console.error(err))
+    .then(result => {
+      this.imageText = result.text;
+    })
+    .finally(resultOrError => {
+      this.progress.complete();
+    });
+  }
 }
